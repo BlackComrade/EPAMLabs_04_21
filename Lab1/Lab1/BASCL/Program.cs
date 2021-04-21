c69296cb081ff6744f79623bbfbda703782b69af
BlackComrade <you@example.com>
2021-04-21 21:23:51 +0300

    GIT HW

---
+++ Lab1/Lab1/BASCL/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BASCL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b;
+            string c;
+            Console.WriteLine("Hello user. \n Enter the first number");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Choose operation");
+            c = Console.ReadLine();
+            Console.Write("Result" + "->");
+            switch (c)
+            {
+                case "+":
+                    Console.WriteLine(a + b);
+                    break;
+                case "-":
+                    Console.WriteLine(a - b);
+                    break;
+                case "*":
+                    Console.WriteLine(a * b);
+                    break;
+                case "/":
+                    if (b == 0)
+                        Console.WriteLine("invalid second number");
+                    else
+                        Console.WriteLine(a / b);
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab1/Lab1/massssssss/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace massiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 28, 5, 6, 21, 1, 8, 2, 11, 7, 81 };
+            Array.Sort(arr);
+            Array.ForEach(arr, Console.WriteLine);
+        }
+    }
+}
