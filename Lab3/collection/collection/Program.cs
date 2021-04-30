c2d59ca0798a7bd3deca0d2e742b58f92dd0e5c8
BlackComrade <you@example.com>
2021-04-30 22:35:24 +0300

    Ready HW Collection

---
+++ Lab3/collection/collection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collection
+{
+    class Program
+    {
+        public static string Value_input()
+        {
+            Console.WriteLine("Введите название религии");
+            Console.Write("-> ");
+            string name_of_relegion = Console.ReadLine();
+            return name_of_relegion;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Religion> religion = new List<Religion>();
+            religion.Add(new Religion() { Name = "Христианство", Number_of_followers = 2293 });
+            religion.Add(new Religion() { Name = "Ислам", Number_of_followers = 1550 });
+            religion.Add(new Religion() { Name = "Иудаизм", Number_of_followers = 15 });
+            religion.Add(new Religion() { Name = "Синтоизм", Number_of_followers = 3 });
+            religion.Add(new Religion() { Name = "Буддизм", Number_of_followers = 470 });
+
+            string name = Value_input();
+            try
+            {
+                bool religionChek = false;
+                for (int i = 0; i < religion.Count; i++)
+                {
+                    if (name == religion[i].Name)
+                    {
+                        religionChek = true;
+                        Console.WriteLine("Количество последователей (млн.чел.): " + religion[i].Number_of_followers);
+                    }
+                }
+                if (religionChek == false) 
+                throw new ReligionExeption("Нет такой религии в списке.");
+            }
+            catch (ReligionExeption ex) 
+            {
+                Console.WriteLine("Это другое " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Lab3/collection/collection/Religion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace collection
+{
+    class Religion
+    {
+        public string Name { get; set; }
+        public int Number_of_followers { get; set; }
+    }
+}
